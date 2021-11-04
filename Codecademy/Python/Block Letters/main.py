357e901d929f36acfd479a7be2a42120c145665b
davempark09 <43024345+davempark09@users.noreply.github.com>
2021-11-03 21:39:47 -0400

    CodeAcademy Practice
    Added a new mini python intro project

---
+++ Codecademy/Python/Block Letters/main.py
@@ -0,0 +1,10 @@
+# David Park
+# Fun Fact: I Love To Learn New Things
+
+print("  DDDD  PPPP  ")
+print("  D   D P   P ")
+print("  D   D P   P ")
+print("  D   D PPPP  ")
+print("  D   D P     ")
+print("  D   D P     ")
+print("  DDDD  P     ")
