02b778fd677f7fe396e01d0cc1dc084a09828a25
davempark09 <43024345+davempark09@users.noreply.github.com>
2021-11-07 23:20:05 -0500

    Added Question/Answer

--- Codecademy/Python/Block Letters/main.py
+++ Codecademy/Python/Block Letters/main.py
@@ -1,3 +1,7 @@
+# ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+#
+# Write a Python program called main.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
+
 # David Park
 # Fun Fact: I Love To Learn New Things
 
